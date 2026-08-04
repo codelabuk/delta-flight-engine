7a8d24f95f91e630bb839c95c5dd673a0c025ca0
Afzal Ahmed <afzal232@gmail.com>
2026-08-05 00:39:51 +0530

    Delta File Add tracker
    Signed-off-by: Afzal Ahmed <afzal232@gmail.com>

---
+++ src/main/java/org/codelabuk/lakehouse/deltaflight/action/AddFile.java
@@ -0,0 +1,49 @@
+package org.codelabuk.lakehouse.deltaflight.action;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.Map;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class AddFile {
+    private String path;
+    private Map<String, String> partitionValues;
+    private long size;
+    private long modificationTime;
+    private boolean dataChange;
+    private String stats;
+
+    public String getPath() {
+        return path;
+    }
+
+    public Map<String, String> getPartitionValues() {
+        return partitionValues == null ? Map.of() : partitionValues;
+    }
+
+    public long getSize() {
+        return size;
+    }
+
+    public long getModificationTime() {
+        return modificationTime;
+    }
+
+
+    public boolean isDataChange() {
+        return dataChange;
+    }
+
+    public String getStats() {
+        return stats;
+    }
+
+    @Override
+    public String toString() {
+        return "AddFile{" +
+                "path='" + path + '\'' +
+                ", partitionValues=" + partitionValues +
+                ", size=" + size +
+                '}';
+    }
+}
